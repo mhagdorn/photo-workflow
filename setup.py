35d56fa3c25851f823f7e6a6409ab4b03c32471e
mhagdorn <magnus.hagdorn@marsupium.org>
2020-05-05 17:07:55 +0100

    add setup script

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='photo-workflow',
+      install_requires = ['easywebdav',],
+      version='0.1',
+      description='marsupium photo workflow helpers',
+      author='Magnus Hagdorn',
+      author_email='magnus.hagdorn@marsupium.org',
+      url='https://github.com/mhagdorn/photo-workflow',
+      packages=['photo_workflow'],
+      entry_points={
+          'console_scripts': [
+              'photo-create-project = photo_workflow.create:main',
+              'photo-backup-project = photo_workflow.backup:main',
+              'photo-download-gpx = photo_workflow.getgpx:main',
+              ],
+      },
+
+  )
